feb77c5296e5ee8abcd2b2fdd3d1e3213d4af145
Adam Priest <sepone229@gmail.com>
2020-03-15 16:56:30 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,16 +1,16 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-      :partriarch => {},
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      }, 
-    :capulet => {
+   },
+   :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
-  }
+   }
+}
 end
