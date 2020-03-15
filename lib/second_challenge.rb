be08646c15115d752d7552431e46de143acd651d
Adam Priest <sepone229@gmail.com>
2020-03-15 16:48:29 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,7 +1,22 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      #your key/value pairs here!
+      :patriarch => {
+        :name => "Lord Montague",
+        :age => "53"
+      },
+      :matriach => {
+        :name => "Lady Montague",
+        :age => "54"
+      },
+      :hero => {
+        :name => "Romeo",
+        :age => "15"
+      },
+      :hero_friends => [
+        {
+          :
+        }]
       }, 
     :capulet => {
       #your key/value pairs here!
