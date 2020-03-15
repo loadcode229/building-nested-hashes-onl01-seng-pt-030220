a2bdb0c6e9d3dbc90c6493eddd31c9a8f0644b3b
Adam Priest <sepone229@gmail.com>
2020-03-15 17:23:09 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -1,4 +1,6 @@
 def fourth_challenge
+      epic_tragedy[:montague][:hero][:status] = "dead", [:capulet][:heroine][:status] = "dead"
+      puts epic_tragedy
   epic_tragedy = {
    :montague => {
       :patriarch => {name: "Lord Montague", age: "53"},
